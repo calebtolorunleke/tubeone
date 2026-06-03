9082307a6b5fc6d83040fe3a2bcbbad8e8992ff0
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-06-02 23:45:09 -0700

    feat: update on the store procedure

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,16 +2,31 @@ import React, { useState } from "react";
 
 const Form = () => {
   //   const [name, setName] = useState("");
-  const [name, setName] = useState({ firstname: "", lastname: "" });
-  const handleChange = (e) => {
-    console.log("change the code ");
-    setName({ firstname: e.target.value, lastname: e.target.value });
+  const [name, setName] = useState({ firstName: "", lastName: "" });
+  //   const handleChange = (e) => {
+  //     console.log("change the code ");
+  //     setName({ ...name, firstname: e.target.value, lastname: e.target.value });
+  //   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
   };
   return (
     <div>
+      {name.firstName} - {name.lastName}
       <form>
-        <input onChange={handleChange} type="text" value={name.firstname} />
-        <input onChange={handleChange} type="text" value={name.lastname} />
+        <input
+          onChange={(e) => setName({ ...name, firstName: e.target.value })}
+          type="text"
+          value={name.firstname}
+        />
+        <input
+          onChange={(e) => setName({ ...name, lastName: e.target.value })}
+          type="text"
+          value={name.lastname}
+        />
+        <button onClick={(e) => handleSubmit(e)}>Add</button>
       </form>
     </div>
   );
